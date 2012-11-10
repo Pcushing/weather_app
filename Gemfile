c69ebe75a954896bd12958de6f10f57668acbce0
Patrick Cushing <patrick.j.cushing@gmail.com>
2012-11-10 13:40:07 -0500

    Adds sendgrid

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ gem 'rails', '3.2.8'
 gem 'twilio-ruby'
 gem 'pg'
 gem 'twitter'
+gem 'sendgrid'
 
 group :assets do
   gem 'sass-rails',   '~> 3.2.3'
--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 WeatherApp::Application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  :user_name => "stormsurge",
+  :password => "stormsurge",
+  :domain => "aqueous-fortress-8839.herokuapp.com/",
+  :address => "smtp.sendgrid.net",
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true,
+}
