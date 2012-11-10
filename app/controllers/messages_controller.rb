ee585c8b0e52c936e254fb821cfea06728c6fcd8
Patrick Cushing <patrick.j.cushing@gmail.com>
2012-11-10 12:58:36 -0500

    New Message

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,11 +1,18 @@
 class MessagesController < ApplicationController
   
   def new
-    @response = Twilio::TwiML::Response.new do |r|
-      r.Sms "Thanks. We've contacted your close contacts. Stay safe!"
-    end
-    respond_to do |format|
-        format.xml { render xml: @response.text }
+    warn "T"*50
+    warn "In new"
+    warn params[:Body]
+    warn params[:From]
+    @message = Message.new(params[:Body], params[:From])    
+    if @message.save
+      @response = Twilio::TwiML::Response.new do |r|
+        r.Sms "Thanks. We've contacted your close contacts. Stay safe!"
+      end
+      respond_to do |format|
+          format.xml { render xml: @response.text }
+      end
     end
   end
   
