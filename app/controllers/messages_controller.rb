892da82862f8f1162bb4525e3828034289359fb6
Patrick Cushing <patrick.j.cushing@gmail.com>
2012-11-10 18:27:15 -0500

    Standardizes create

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,8 +1,7 @@
 class MessagesController < ApplicationController
   
   def new
-    @body = params[:Body]
-    @message = Message.new(body: @body, phone: params[:From])    
+    @message = Message.new(body: params[:Body], phone: params[:From])    
     if @message.save
       @response = Twilio::TwiML::Response.new do |r|
         r.Sms "Thanks. We've contacted your close contacts. Stay safe!"
