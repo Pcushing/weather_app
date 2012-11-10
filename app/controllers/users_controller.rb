c75b8a4a8bae94e96409e219e298fa583dd3a771
Patrick Cushing <patrick.j.cushing@gmail.com>
2012-11-10 15:25:25 -0500

    Makes sure contacts exist, responds

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,19 +6,15 @@ class UsersController < ApplicationController
   end
   
   def create
+    # YIKES this is bad code
     request = JSON.parse(params.to_json).symbolize_keys!
     user_params = request[:user]
-    contacts_params = request[:contacts]
-    warn "T"*50
-    warn contacts_params.inspect
-    
-    @user = User.new(phone: user_params["phone"],
-                     email: user_params["email"]
-                     )
+    contacts_params = request[:contacts] 
+    @user = User.new(phone: user_params["phone"], email: user_params["email"])
     respond_to do |format|
        if @user.save
-         @user.create_contacts contacts_params
-         format.json { render json: @item, status: :created, location: @item }
+         @user.create_contacts(contacts_params) if !contacts_params.nil?
+         format.json { render json: @user.to_json, status: :created, location: @user }
        else
          format.json { 'something went wrong' }
        end 
