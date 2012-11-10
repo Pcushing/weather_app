0c5b6bbf0d9ea12e0688312ce6de488d4bd278a2
Patrick Cushing <patrick.j.cushing@gmail.com>
2012-11-10 15:12:53 -0500

    Creates contacts from json post

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,16 +6,18 @@ class UsersController < ApplicationController
   end
   
   def create
-    warn "T"*50
-    warn params.to_s
     request = JSON.parse(params.to_json).symbolize_keys!
     user_params = request[:user]
-    warn user_params
+    contacts_params = request[:contacts]
+    warn "T"*50
+    warn contacts_params.inspect
+    
     @user = User.new(phone: user_params["phone"],
                      email: user_params["email"]
                      )
     respond_to do |format|
        if @user.save
+         @user.create_contacts contacts_params
          format.json { render json: @item, status: :created, location: @item }
        else
          format.json { 'something went wrong' }
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,10 @@ class User < ActiveRecord::Base
   attr_accessible :email, :name, :phone
   validates_presence_of :phone, :on => :create, :message => "can't be blank"
   has_many :contacts
+  
+  def create_contacts(contacts_params)
+    contacts_params.each do |contact|
+      self.contacts.create contact
+    end
+  end
 end
