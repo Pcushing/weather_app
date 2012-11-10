13d4f3f5d5f854f76e92af88111685228309dee4
Patrick Cushing <patrick.j.cushing@gmail.com>
2012-11-10 12:40:11 -0500

    Adds sms to contacts on message create

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,11 +3,10 @@ class MessagesController < ApplicationController
   def new
     warn "T"*50
     warn "In new"
-    warn params.inspect
     warn params[:Body]
     warn params[:From]
-    # @message.create(params[:Body], params[:From])    
-    @response = Twilio::TwiML::Response.new do |r|
+    @message.create(params[:Body], params[:From])    
+    @response = Twilio::TwherokiML::Response.new do |r|
       r.Sms "Thanks. We've contacted your close contacts. Stay safe!"
     end
     respond_to do |format|
@@ -16,17 +15,5 @@ class MessagesController < ApplicationController
   end
   
   def create
-    warn "T"*50
-    warn "In create"
-    warn params.inspect
-    warn params[:Body]
-    warn params[:From]
-    @message.create(params[:Body], params[:From])    
-    @response = Twilio::TwiML::Response.new do |r|
-      r.Sms "Thanks. We've contacted your close contacts. Stay safe!"
-    end
-    respond_to do |format|
-        format.xml { render xml: @response.text }
-    end
   end
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,3 +1,23 @@
 class Message < ActiveRecord::Base
   attr_accessible :body, :phone
+  
+  after_create :sms_contacts
+  
+  private
+    def sms_contacts
+      account_sid = 'ACa2fd753d813c6b10be9a38af65653e6b'
+      auth_token = '2601077aa3fa59bbc7a6c5459f8beafc'
+      
+      @client = Twilio::REST::Client.new account_sid, auth_token
+      @user = User.find_by_phone(self.phone)  
+      if !@user.contacts.empty?  
+        @user.contacts.each do |contact|
+          @client.account.sms.messages.create(
+            :from => '+19789653430',
+            :to => contact.phone,
+            :body => self.body
+          )
+        end
+      end
+    end
 end
