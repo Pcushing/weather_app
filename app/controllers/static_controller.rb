9e39f04761c841de1dc3985e7249c7d6eb1a1695
Patrick Cushing <patrick.j.cushing@gmail.com>
2012-11-10 12:08:15 -0500

    again

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -3,6 +3,11 @@ class StaticController < ApplicationController
   end
   
   def receiver
+    warn "T"*50
+    warn params.inspect
+    warn params[:Body]
+    warn params[:From]
+    
     account_sid = 'ACa2fd753d813c6b10be9a38af65653e6b'
     auth_token = '2601077aa3fa59bbc7a6c5459f8beafc'
     
@@ -11,14 +16,15 @@ class StaticController < ApplicationController
     @sms_text = params[:Body]
     @sms_number = params[:From]
     
-    @user = User.find_by_phone(@sms_number)    
-    @user.contacts.each do |contact|
-      @client.account.sms.messages.create(
-        :from => '+19789653430',
-        :to => contact.phone,
-        :body => @sms_text
-      )
-    end
+    @user = User.find_by_phone(@sms_number)  
+    warn @user.inspect  
+    # @user.contacts.each do |contact|
+    #   @client.account.sms.messages.create(
+    #     :from => '+19789653430',
+    #     :to => contact.phone,
+    #     :body => @sms_text
+    #   )
+    # end
     
     @response = Twilio::TwiML::Response.new do |r|
       r.Sms "Thanks. We've contacted your close contacts. Stay safe!"
