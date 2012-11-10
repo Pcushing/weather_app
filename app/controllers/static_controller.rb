36a71f3b4666f19deceefcb5ea917b03e0b77626
Patrick Cushing <patrick.j.cushing@gmail.com>
2012-11-10 10:32:56 -0500

    Logs params

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,10 +1,8 @@
 class StaticController < ApplicationController
   def home
-    # put your own credentials here
     account_sid = 'ACa2fd753d813c6b10be9a38af65653e6b'
     auth_token = '2601077aa3fa59bbc7a6c5459f8beafc'
 
-    # set up a client to talk to the Twilio REST API
     @client = Twilio::REST::Client.new account_sid, auth_token
     @client.account.sms.messages.create(
       :from => '+19789653430',
@@ -14,12 +12,12 @@ class StaticController < ApplicationController
   end
   
   def receiver
-    response = Twilio::TwiML::Response.new do |r|
-      r.Sms 'hello there'
-    end
+    puts params.inspect
     
-    puts response.inspect
-    puts response.text
+    response = Twilio::TwiML::Response.new do |r|
+      r.Sms 'well hello there'
+    end
+
     respond_to do |format|
         format.xml { render xml: response.text }
     end
