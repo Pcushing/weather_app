39b4611bc5dbd2214bb4475cec258a568c44c495
Patrick Cushing <patrick.j.cushing@gmail.com>
2012-11-10 11:38:01 -0500

    cmon

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -3,28 +3,33 @@ class StaticController < ApplicationController
   end
   
   def receiver
-    account_sid = 'ACa2fd753d813c6b10be9a38af65653e6b'
-    auth_token = '2601077aa3fa59bbc7a6c5459f8beafc'
-    
-    @client = Twilio::REST::Client.new account_sid, auth_token
-    
-    @sms_text = params[:body]
-    @sms_number = params[:from]
+    # account_sid = 'ACa2fd753d813c6b10be9a38af65653e6b'
+    # auth_token = '2601077aa3fa59bbc7a6c5459f8beafc'
+    # 
+    # @client = Twilio::REST::Client.new account_sid, auth_token
+    # 
+    # @sms_text = params[:body]
+    # @sms_number = params[:from]
+    # 
+    # warn "T"*50
+    # warn params[:from]
+    # warn params[:body]
+    # 
+    # @user = User.find_by_phone(@sms_number)
+    # warn @user
+    # 
+    # @user.contacts.each do |contact|
+    #   @client.account.sms.messages.create(
+    #     :from => '+19789653430',
+    #     :to => contact.phone,
+    #     :body => @sms_text
+    #   )
+    # end
     
     warn "T"*50
+    warn params.inspect
     warn params[:from]
     warn params[:body]
-    
-    @user = User.find_by_phone(@sms_number)
-    warn @user
-    
-    @user.contacts.each do |contact|
-      @client.account.sms.messages.create(
-        :from => '+19789653430',
-        :to => contact.phone,
-        :body => @sms_text
-      )
-    end
     
     @response = Twilio::TwiML::Response.new do |r|
       r.Sms "Thanks. We've contacted your close contacts. Stay safe!"
